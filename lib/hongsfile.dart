49a4ebd010cebf89b08917b59507007c7218d30f
hongcheol <hongcheol6689@gmail.com>
2024-04-10 12:08:34 +0900

    main page changed

---
+++ lib/hongsfile.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HongsFile extends StatelessWidget {
+  const HongsFile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("hong's file~!"),
+      ),
+    );
+  }
+}
